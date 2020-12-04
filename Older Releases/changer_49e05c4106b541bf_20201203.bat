9ec8d5948a3c186981b5f0efc853434d319e1255
Over-Alphine <75469607+Over-Alphine@users.noreply.github.com>
2020-12-04 18:41:31 +0900

    Update changer_49e05c4106b541bf_20201203.bat

--- Older Releases/changer_49e05c4106b541bf_20201203.bat
+++ Older Releases/changer_49e05c4106b541bf_20201203.bat
@@ -22,12 +22,12 @@ copy ui.ttf ui2.ttf
 copy ui.ttf ui3.ttf
 copy ui.ttf ui4.ttf
 copy killfeed.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\arial.ttf
-copy killfeed.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\arialbd.ttf
+copy killfeed1.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\arialbd.ttf
 copy ui.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-Bold.ttf
-copy ui.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-It.ttf
-copy ui.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-Light.ttf
-copy ui.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-Regular.ttf
-copy ui.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-Semibold.ttf
+copy ui1.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-It.ttf
+copy ui2.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-Light.ttf
+copy ui3.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-Regular.ttf
+copy ui4.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-Semibold.ttf
 start delete.bat
 exit
 
