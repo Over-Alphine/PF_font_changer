629ccc41c668754a7fd5d7e0ba8e7d03957f690d
Over-Alphine <75469607+Over-Alphine@users.noreply.github.com>
2020-12-04 16:14:33 +0900

    Update changer.bat

--- changer.bat
+++ changer.bat
@@ -5,6 +5,5 @@ move ui.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf
 move ui.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-Light.ttf
 move ui.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-Regular.ttf
 move ui.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-49e05c4106b541bf\content\fonts\SourceSansPro-Semibold.ttf
-
 start delete.bat
 exit
