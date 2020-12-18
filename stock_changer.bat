08c1c2f18566431cd94b130f4ea93e7ae4c6a6a0
Over-Alphine <75469607+Over-Alphine@users.noreply.github.com>
2020-12-18 22:46:37 +0900

    Update stock_changer.bat

--- stock_changer.bat
+++ stock_changer.bat
@@ -1,10 +1,10 @@
-wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/arial.ttf?raw=true
-wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/arialbd.ttf?raw=true
-wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Bold.ttf?raw=true
-wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-It.ttf?raw=true
-wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Light.ttf?raw=true
-wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Regular.ttf?raw=true
-wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Semibold.ttf?raw=true
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/arial.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/arialbd.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Bold.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-It.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Light.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Regular.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Semibold.ttf
 copy arial.ttf C:\Program Files (x86)\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arial.ttf
 copy arialbd.ttf C:\Program Files (x86)\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arialbd.ttf
 copy SourceSansPro-Bold.ttf C:\Program Files (x86)\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Bold.ttf
