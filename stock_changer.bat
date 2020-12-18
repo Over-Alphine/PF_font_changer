8d34f04c183691458d29bfae4c3bd76c3e31db04
Over-Alphine <75469607+Over-Alphine@users.noreply.github.com>
2020-12-18 22:42:13 +0900

    Add files via upload

---
+++ stock_changer.bat
@@ -0,0 +1,16 @@
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/arial.ttf?raw=true
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/arialbd.ttf?raw=true
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Bold.ttf?raw=true
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-It.ttf?raw=true
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Light.ttf?raw=true
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Regular.ttf?raw=true
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Semibold.ttf?raw=true
+copy arial.ttf C:\Program Files (x86)\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arial.ttf
+copy arialbd.ttf C:\Program Files (x86)\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arialbd.ttf
+copy SourceSansPro-Bold.ttf C:\Program Files (x86)\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Bold.ttf
+copy SourceSansPro-It.ttf C:\Program Files (x86)\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-It.ttf
+copy SourceSansPro-Light.ttf C:\Program Files (x86)\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Light.ttf
+copy SourceSansPro-Regular.ttf C:\Program Files (x86)\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Regular.ttf
+copy SourceSansPro-Semibold.ttf C:\Program Files (x86)\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Semibold.ttf
+start stock_delete.bat
+exit
