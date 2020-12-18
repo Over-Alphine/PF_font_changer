792718ae1bef99734409379f296c3f7350722cd5
Over-Alphine <75469607+Over-Alphine@users.noreply.github.com>
2020-12-18 22:14:48 +0900

    Update changer_7b3e1cf091a4470b_20201212.bat

--- Older Releases/changer_7b3e1cf091a4470b_20201212.bat
+++ Older Releases/changer_7b3e1cf091a4470b_20201212.bat
@@ -9,45 +9,77 @@ set /p confirm=Please Press Y Then Enter To Proceed, Press Other Key To Quit :
 echo.
 
 
-if "%confirm%"=="y" goto EXECUTE
+if "%confirm%"=="y" goto Main2
 if not "%confirm%"=="y" goto ABORT
 
 
-:EXECUTE
+:Main2
 color f1
 mode con cols=80 lines=20
-copy killfeed.ttf killfeed1.ttf
+@echo off 
+echo       Do you want to use single font?
+echo          Press 1 to use single font.
+echo         Press 2 to use double font.
+set /p count=Please enter your answer.
+echo.
+
+if "%count%"=="1" goto SINGLE1
+if "%count%"=="2" goto DOUBLE1
+
+:SINGLE1
+echo      Now rename your font to UI.ttf and press Y, then enter to proceed.
+set /p confirm=Please Press Y Then Enter To Proceed, Press Other Key To Quit : 
+echo.
+
+if "%confirm%"=="y" goto SINGLE2
+if not "%confirm%"=="y" goto ABORT
+
+:SINGLE2
+copy ui.ttf killfeed1.ttf
+copy ui.ttf killfeed2.ttf
 copy ui.ttf ui1.ttf
 copy ui.ttf ui2.ttf
 copy ui.ttf ui3.ttf
 copy ui.ttf ui4.ttf
-copy killfeed.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arial.ttf
-copy killfeed1.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arialbd.ttf
-copy ui.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Bold.ttf
-copy ui1.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-It.ttf
-copy ui2.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Light.ttf
-copy ui3.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Regular.ttf
-copy ui4.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Semibold.ttf
+copy ui.ttf ui5.ttf
+copy killfeed1.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arial.ttf
+copy killfeed2.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arialbd.ttf
+copy ui1.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Bold.ttf
+copy ui2.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-It.ttf
+copy ui3.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Light.ttf
+copy ui4.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Regular.ttf
+copy ui5.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Semibold.ttf
+start delete.bat
+exit
+
+:DOUBLE1
+echo      Now rename your one font to UI.ttf, another font to killfeed.ttf and press Y, then enter to proceed.
+set /p confirm=Please Press Y Then Enter To Proceed, Press Other Key To Quit : 
+echo.
+
+if "%confirm%"=="y" goto DOUBLE2
+if not "%confirm%"=="y" goto ABORT
+
+
+:DOUBLE2
+copy killfeed.ttf killfeed1.ttf
+copy killfeed.ttf killfeed2.ttf
+copy ui.ttf ui1.ttf
+copy ui.ttf ui2.ttf
+copy ui.ttf ui3.ttf
+copy ui.ttf ui4.ttf
+copy ui.ttf ui5.ttf
+copy killfeed1.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arial.ttf
+copy killfeed2.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arialbd.ttf
+copy ui1.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Bold.ttf
+copy ui2.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-It.ttf
+copy ui3.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Light.ttf
+copy ui4.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Regular.ttf
+copy ui5.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Semibold.ttf
 start delete.bat
 exit
 
 :ABORT
 echo Process Aborted By User Request. Terminating..
 timeout 3 /nobreak
-exit
-
-
-copy killfeed.ttf killfeed1.ttf
-copy ui.ttf ui1.ttf
-copy ui.ttf ui2.ttf
-copy ui.ttf ui3.ttf
-copy ui.ttf ui4.ttf
-copy killfeed.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arial.ttf
-copy killfeed1.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\arialbd.ttf
-copy ui.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Bold.ttf
-copy ui1.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-It.ttf
-copy ui2.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Light.ttf
-copy ui3.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Regular.ttf
-copy ui4.ttf C:\Users\User\AppData\Local\Roblox\Versions\version-7b3e1cf091a4470b\content\fonts\SourceSansPro-Semibold.ttf
-start delete.bat
 exit
