ecff15176b49c6eb8b62c667e7b2de51dd70c9f8
Over-Alphine <75469607+Over-Alphine@users.noreply.github.com>
2021-02-04 21:57:52 +0900

    Add files via upload

--- stock_changer.bat
+++ stock_changer.bat
@@ -2,7 +2,38 @@ color f1
 mode con cols=80 lines=20
 @echo off
 
-:: BatchGotAdmin
+echo Select installer option
+echo If your Roblox executable is located under Program files, type 1.
+echo If your Roblox executable is located under Appdata, type 2.
+echo Wrong selection in this stage will result in failure. Be wary.
+set /p instopt=Please select installer option.
+if "%instopt%"=="1" goto BatchGotAdmin
+if "%instopt%"=="2" goto User
+
+:User
+echo Please type your account name (ex. user)
+set /p usrname=Type your local Windows account name...
+goto Nonadmin
+
+:Nonadmin
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/arial.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/arialbd.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Bold.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-It.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Light.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Regular.ttf
+wget https://github.com/Over-Alphine/PF_font_changer/blob/main/Stock/SourceSansPro-Semibold.ttf
+copy "arial.ttf" "C:\Users\%usrname%\AppData\Local\Roblox\Versions\version-29d2a7fa87dd447b\content\fonts\arial.ttf"
+copy "arialbd.ttf" "C:\Users\%usrname%\AppData\Local\Roblox\Versions\version-29d2a7fa87dd447b\content\fonts\arialbd.ttf"
+copy "SourceSansPro-Bold.ttf" "C:\Users\%usrname%\AppData\Local\Roblox\Versions\version-29d2a7fa87dd447b\content\fonts\SourceSansPro-Bold.ttf"
+copy "SourceSansPro-It.ttf" "C:\Users\%usrname%\AppData\Local\Roblox\Versions\version-29d2a7fa87dd447b\content\fonts\SourceSansPro-It.ttf"
+copy "SourceSansPro-Light.ttf" "C:\Users\%usrname%\AppData\Local\Roblox\Versions\version-29d2a7fa87dd447b\content\fonts\SourceSansPro-Light.ttf"
+copy "SourceSansPro-Regular.ttf" "C:\Users\%usrname%\AppData\Local\Roblox\Versions\version-29d2a7fa87dd447b\content\fonts\SourceSansPro-Regular.ttf"
+copy "SourceSansPro-Semibold.ttf" "C:\Users\%usrname%\AppData\Local\Roblox\Versions\version-29d2a7fa87dd447b\content\fonts\SourceSansPro-Semibold.ttf"
+start stock_delete.bat
+exit
+
+:BatchGotAdmin
 :-------------------------------------
 REM  --> Check for permissions
     IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
